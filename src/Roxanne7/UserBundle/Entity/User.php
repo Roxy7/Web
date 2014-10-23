d8f48b6c48effb8f6b16ea14aa961e765877b681
Roxanne7_ <Roxanne7_@collectifembriagoun.com>
2014-10-23 14:47:00 +0200

    Ovveriding FOSUserBundle registration form adding "name" attribute

--- src/Roxanne7/UserBundle/Entity/User.php
+++ src/Roxanne7/UserBundle/Entity/User.php
@@ -28,6 +28,21 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max="255",
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $name;
+    
+    
     public function __construct()
     {
     	parent::__construct();
@@ -37,4 +52,37 @@ class User extends BaseUser
     
     
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
